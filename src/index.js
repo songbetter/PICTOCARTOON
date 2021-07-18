fa8ef9def1ff0329fda69c45c6e0d9987f4b9ce4
songbetter <5ongnaeu17@gmail.com>
2021-07-19 01:03:07 +0900

    개발환경세팅: Redux 추가

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../src/lib/styles/globalstyles';
 import { theme } from '../src/lib/styles/theme';
 import Routes from './Routes';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
