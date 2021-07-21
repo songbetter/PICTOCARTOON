f0d39dc1cbd3550b19a9e85266575c90fe321337
songbetter <5ongnaeu17@gmail.com>
2021-07-21 22:35:20 +0900

    Item 컴포넌트 버그 수정 (return)

--- src/components/molecules/Item.js
+++ src/components/molecules/Item.js
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import FlexRowBetween from '../atoms/FlexRowBetween';
+import Title from '../atoms/Title';
 
 const Item = (props) => {
-  <FlexRowBetween>
-    <Link to={`mypage/order/${props.id}`}>
-      {props.id} {props.name}
-    </Link>
-  </FlexRowBetween>;
+  return (
+    <ItemWrapper>
+      <div>
+        <Title>주문번호: </Title>
+        {props.id}
+      </div>
+      <Link to={`/mypage/order/${props.id}`}>{props.name}</Link>
+    </ItemWrapper>
+  );
 };
 
 export default Item;
+
+const ItemWrapper = styled(FlexRowBetween)`
+  padding: 0.8rem;
+  margin: 0.25rem;
+  border: solid 1px ${(props) => props.theme.colors.sub};
+  width: 100%;
+`;
